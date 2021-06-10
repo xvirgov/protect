c89cfc3f7d7ee8d19ebdb433ea0c8b6e9103b16f
Michal <michal.virgovic1@gmail.com>
2021-06-10 12:38:44 +0200

    Change extract stats

--- bin/extract-stats.sh
+++ bin/extract-stats.sh
@@ -46,14 +46,35 @@ RSA_MEASUREMENTS=(
   RsaRefreshAssemblePolynomialEnd
   RsaRefreshTotalEnd)
 
+KYBER_MEASUREMENTS=(
+  # Info
+  KyberInfoGet
+
+  # Gen
+  KyberStoreEnd
+  KyberGenEnd
+  KyberGenTotal
+
+  # Enc
+  KyberEncEnd
+
+  # Dec
+  KyberDecEnd)
+
 for (( i=0; i<${#ECIES_MEASUREMENTS[@]}; i++ ));
 do
   ARR=$(cat $LOGFILE | grep "PerfMeas:${ECIES_MEASUREMENTS[$i]}" | grep -Eo '[0-9]+$')
-  echo $ARR | sed 's/ /,/g' > "extracted/${ECIES_MEASUREMENTS[$i]}${LABEL}.csv"
+  [[ ! -z $ARR ]] && echo $ARR | sed 's/ /,/g' > "extracted/${ECIES_MEASUREMENTS[$i]}${LABEL}.csv"
 done
 
 for (( i=0; i<${#RSA_MEASUREMENTS[@]}; i++ ));
 do
   ARR=$(cat $LOGFILE | grep "PerfMeas:${RSA_MEASUREMENTS[$i]}" | grep -Eo '[0-9]+$')
-  echo $ARR | sed 's/ /,/g' > "extracted/${RSA_MEASUREMENTS[$i]}${LABEL}.csv"
+  [[ ! -z $ARR ]] && echo $ARR | sed 's/ /,/g' > "extracted/${RSA_MEASUREMENTS[$i]}${LABEL}.csv"
+done
+
+for (( i=0; i<${#KYBER_MEASUREMENTS[@]}; i++ ));
+do
+  ARR=$(cat $LOGFILE | grep "PerfMeas:${KYBER_MEASUREMENTS[$i]}" | grep -Eo '[0-9]+$')
+  [[ ! -z $ARR ]] && echo $ARR | sed 's/ /,/g' > "extracted/${KYBER_MEASUREMENTS[$i]}${LABEL}.csv"
 done
